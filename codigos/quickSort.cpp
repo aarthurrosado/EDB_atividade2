00c6b3d89ea7f5e2c638ee97e6bbbd0e91626580
Arthur Rosado <aarthurrosado@gmail.com>
2025-10-05 20:00:48 -0300

    Update quickSort.cpp

--- codigos/quickSort.cpp
+++ codigos/quickSort.cpp
@@ -3,50 +3,49 @@
 using namespace std;
 
 int particiona(int arr[], int inicio, int fim, int &comparacoes, int &trocas) {
-    int pivo = arr[(inicio + fim) / 2]; // peguei o do meio pra variar
-    int i = inicio;
-    int j = fim;
+    int pivo = arr[(inicio + fim) / 2]; // escolhe o elemento do meio como pivô
+    int i = inicio; // ponteiro que avança da esquerda para a direita
+    int j = fim;    // ponteiro que recua da direita para a esquerda
 
     while (i <= j) {
-        // anda com o i até achar algo maior que o pivô
+        // avança i enquanto o elemento for menor que o pivô
         while (arr[i] < pivo) {
-            comparacoes++;
+            comparacoes++; // conta cada comparação feita
             i++;
         }
 
-        // anda com o j até achar algo menor que o pivô
+        // recua j enquanto o elemento for maior que o pivô
         while (arr[j] > pivo) {
-            comparacoes++;
+            comparacoes++; // conta cada comparação feita
             j--;
         }
 
-        // se cruzaram, hora de trocar
+        // quando os ponteiros se cruzam, faz a troca se necessário
         if (i <= j) {
-            if (i != j) {
+            if (i != j) { // evita trocar o mesmo elemento
                 int aux = arr[i];
                 arr[i] = arr[j];
                 arr[j] = aux;
-                trocas++;
+                trocas++; // incrementa o contador de trocas
             }
             i++;
             j--;
         }
     }
 
-    return i; // nova posição de corte
+    return i; // retorna o índice onde o vetor será dividido
 }
 
-// A função recursiva que faz a “bagunça virar ordem”
+// função recursiva responsável por ordenar as partições
 void quickSort(int arr[], int inicio, int fim, int &comparacoes, int &trocas) {
-    if (inicio < fim) {
-        int indice = particiona(arr, inicio, fim, comparacoes, trocas);
-        // resolve o lado esquerdo
-        quickSort(arr, inicio, indice - 1, comparacoes, trocas);
-        // e o direito
-        quickSort(arr, indice, fim, comparacoes, trocas);
+    if (inicio < fim) { // condição de parada da recursão
+        int indice = particiona(arr, inicio, fim, comparacoes, trocas); // particiona o vetor
+        quickSort(arr, inicio, indice - 1, comparacoes, trocas); // ordena a parte esquerda
+        quickSort(arr, indice, fim, comparacoes, trocas);        // ordena a parte direita
     }
 }
 
+/*
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -69,3 +68,4 @@ int main() {
 
     return 0;
 }
+*/
