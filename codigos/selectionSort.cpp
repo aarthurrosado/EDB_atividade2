e017b90e59611d246b5ae4a896149f3259492166
Arthur Rosado <aarthurrosado@gmail.com>
2025-10-05 20:01:52 -0300

    Update selectionSort.cpp

--- codigos/selectionSort.cpp
+++ codigos/selectionSort.cpp
@@ -2,47 +2,56 @@
 #include <ctime>
 using namespace std;
 
-void selectionsort(int arr[], int n, int &comparacoes, int &trocas) {
-    comparacoes = 0;
-    trocas = 0;
-
-    // Percorre o vetor todo
-    for (int i = 0; i < n - 1; i++) {
-        int menor = i; // guarda a posição do menor valor
-
-        // procura o menor elemtento
-        for (int j = i + 1; j < n; j++) {
-            comparacoes++;
-            if (arr[j] < arr[menor]) {
-                menor = j;
+void selectionSort(int v[], int tam, int &comp, int &troca) {
+    comp = 0;   // zera o contador de comparações
+    troca = 0;  // zera o contador de trocas
+    // percorre o vetor até o penúltimo elemento
+    for (int i = 0; i < tam - 1; i++) {
+        int posMenor = i; // guarda a posição do menor elemento encontrado
+        // procura o menor elemento no restante do vetor
+        for (int j = i + 1; j < tam; j++) {
+            comp++; // incrementa a contagem de comparações
+            if (v[j] < v[posMenor]) { // verifica se encontrou um valor menor
+                posMenor = j; // atualiza a posição do menor elemento
             }
         }
-      // troca o menor elemento
-      if (menor != i) {
-            int aux = arr[i];
-            arr[i] = arr[menor];
-            arr[menor] = aux;
-            trocas++;
+        // troca o elemento atual pelo menor encontrado
+        if (posMenor != i) {
+            int temp = v[i];
+            v[i] = v[posMenor];
+            v[posMenor] = temp;
+            troca++; // incrementa o número de trocas realizadas
         }
     }
 }
 
+/*
 int main() {
-    int dados[] = {1, 2, 40, 21, 15, 99, 3, 6, 90};
-    int n = sizeof(dados) / sizeof(dados[0]);
-    int comparacoes, trocas;
-    clock_t inicio = clock();
-    selectionSort(dados, n, comparacoes, trocas);
+    int nums[] = {32, 11, 45, 8, 19, 27, 2, 6};
+    int n = sizeof(nums) / sizeof(nums[0]);
+    int comparas, troc;
+
+    cout << "ANTES -> ";
+    for (int i = 0; i < n; i++) cout << nums[i] << " ";
+    cout << "\n\n";
+
+    clock_t ini = clock();
+    selectionSort(nums, n, comparas, troc);
     clock_t fim = clock();
-    double tempo = double(fim - inicio) / CLOCKS_PER_SEC * 1000;
-    cout << "Vetor ordenado: ";
+
+    double t = double(fim - ini) / CLOCKS_PER_SEC * 1000;
+
+    cout << "Depois da ordenacao:\n";
     for (int i = 0; i < n; i++) {
-        cout << dados[i] << " ";
+        cout << nums[i];
+        if (i < n-1) cout << ",";
     }
     cout << "\n";
-    cout << "Comparacoes: " << comparacoes << endl;
-    cout << "Trocas: " << trocas << endl;
-    cout << "Tempo de execucao: " << tempo << " ms" << endl;
+
+    cout << "Comparacoess : " << comparas << endl;
+    cout << "Troca(s) : " << troc << endl;
+    cout << "Tempo total aproximado : " << t << " ms" << endl;
 
     return 0;
 }
+*/
