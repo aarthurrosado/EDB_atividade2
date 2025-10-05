8e87feeb6fdaecbcf3f165a559ce13f889cccdb2
Arthur Rosado <aarthurrosado@gmail.com>
2025-10-05 20:26:58 -0300

    Update selectionSort.cpp

--- codigos/selectionSort.cpp
+++ codigos/selectionSort.cpp
@@ -2,7 +2,7 @@
 #include <ctime>
 using namespace std;
 
-void selectionSort(int v[], int tam, int &comp, int &troca) {
+void selectionsort(int v[], int tam, int &comp, int &troca) {
     comp = 0;   // zera o contador de comparações
     troca = 0;  // zera o contador de trocas
     // percorre o vetor até o penúltimo elemento
@@ -27,30 +27,30 @@ void selectionSort(int v[], int tam, int &comp, int &troca) {
 
 /*
 int main() {
-    int nums[] = {32, 11, 45, 8, 19, 27, 2, 6};
+    int nums[] = {32, 18, 45, 5, 3, 22, 21, 90, 99, 2};
     int n = sizeof(nums) / sizeof(nums[0]);
     int comparas, troc;
 
-    cout << "ANTES -> ";
+    cout << "antes ";
     for (int i = 0; i < n; i++) cout << nums[i] << " ";
     cout << "\n\n";
 
     clock_t ini = clock();
-    selectionSort(nums, n, comparas, troc);
+    selectionsort(nums, n, comparas, troc);
     clock_t fim = clock();
 
     double t = double(fim - ini) / CLOCKS_PER_SEC * 1000;
 
-    cout << "Depois da ordenacao:\n";
+    cout << "Apos a ordenação \n";
     for (int i = 0; i < n; i++) {
         cout << nums[i];
         if (i < n-1) cout << ",";
     }
     cout << "\n";
 
-    cout << "Comparacoess : " << comparas << endl;
-    cout << "Troca(s) : " << troc << endl;
-    cout << "Tempo total aproximado : " << t << " ms" << endl;
+    cout << "Comparacoes : " << comparas << endl;
+    cout << "Trocas : " << troc << endl;
+    cout << "Tempo total: " << t << " ms" << endl;
 
     return 0;
 }
